4d5faae6c6df4e5d25c167240280773ec0bed1fd
Luke Dando <lukedando@hotmail.co.uk>
2023-10-04 00:13:44 +0100

    Update

--- miryoku/custom_config_0.h
+++ miryoku/custom_config_0.h
@@ -1,6 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define U_LT(LAYER, TAP) &u_lt LAYER TAP
+
+#if defined (MIRYOKU_LAYOUTMAPPING_CORNE)
+
 #define XXX &none
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
@@ -9,10 +13,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kt CAPS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+ &kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+&kt Caps  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 &kp LGUI  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+                   K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -27,11 +31,24 @@
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 37 38
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
 
-#define MIRYOKU_LAYERS_FLIP
 #define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_LAYERS_FLIP
 
-#define MIRYOKU_LAYER_BASE \
-&kp ESC, &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-&kt CAPS, U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-&kp LGUI, U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_FUN, ESC),  U_LT(U_NUM, SPACE), U_LT(U_SYM, TAB),  U_LT(U_MOUSE, RET),U_LT(U_NAV, BSPC),U_LT(U_MEDIA, DEL),U_NP,              U_NP
+#endif
+
+#if defined (MIRYOKU_LAYOUTMAPPING_CORNE)
+
+#define XXX &none
+
+#define MIRYOKU_LAYOUTMAPPING_BASE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+ &kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+&kt Caps  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+&kp LGUI  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+U_LT(U_FUN, ESC)  U_LT(U_NUM, SPACE)  U_LT(U_SYM, TAB)  U_LT(U_MOUSE, RET)  U_LT(U_NAV, BSPC)  U_LT(U_MEDIA, DEL)
+
+#endif
