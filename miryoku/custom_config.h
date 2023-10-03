04fa26b21b3fcad698a0c8a25c2892ec1ffdc571
Luke Dando <lukedando@hotmail.co.uk>
2023-10-04 00:18:17 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -30,7 +30,7 @@
 #define MIRYOKU_LAYERS_FLIP
 #define MIRYOKU_NAV_INVERTEDT
 
-#define MIRYOKU_LAYER_BASE \
+//#define MIRYOKU_LAYER_BASE \
 &kp ESC, &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 &kt CAPS, U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 &kp LGUI, U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
